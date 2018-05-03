c04b598b5a231337d96644dcdceda89250df5563
Lorniebee <lorniebrown@hotmail.com>
2018-05-03 14:37:37 +0200

    added comments

--- solution1.js
+++ solution1.js
@@ -1,12 +1,15 @@
 function spinalCase(str) {
     // "It's such a fine line between stupid, and clever."
     // --David St. Hubbins
-    var regEx1 = /[a-z](?=[A-Z])/g;
-    var regEx2 = /\ |\_/g;
+    var regEx1 = /[a-z](?=[A-Z])/g; // find all lower case immediately followed by upper case
+    var regEx2 = /\ |\_/g;// find all spaces and under scores
+    // if string matches regEx2 replace space or underscore with "-"
     if(str.match(regEx2)) {
       str = str.replace(regEx2, "-");
     }
+    // take modified string and replace regEx1 matches with the match itself followed by "-"
      str = str.replace(regEx1, "$&-");
+    // change all capitals to lower case
     return str.toLowerCase();
   }
   
